f1a934c5ead448fd4bb4e2b3653d555fcd30baef
rjain-pivotal <rajesh.jain@pivotal.io>
2017-02-03 12:20:03 -0500

    Fixing Script Errors

--- ci/tasks/init/stop-env.sh
+++ ci/tasks/init/stop-env.sh
@@ -23,4 +23,23 @@ gcloud config set project $gcp_proj_id
 gcloud config set compute/region $gcp_region
 
 
+# stop all GCP Instances for given prefix within Zone ##MG Todo: Serial processing is slow,  look for a quicker way to stop Instances
+declare -a ZONE=(
+$gcp_zone_1
+$gcp_zone_2
+$gcp_zone_3
+)
+
+for y in ${ZONE[@]}; do
+	echo "----------------------------------------------------------------------------------------------"
+  echo "Will stop all compute/instances objects with the prefix=$gcp_terraform_prefix in:"
+	echo "project=$gcp_proj_id"
+	echo "region=$gcp_region"
+	echo "zone=$y"
+	echo "----------------------------------------------------------------------------------------------"
+  echo "Looking for bosh instance(s) first ...."
+  echo "----------------------------------------------------------------------------------------------"
+
+done
+
 #################### Stop End   ##########################
